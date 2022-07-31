ffa030a0006202b3f8b3fdc50bf2377b4257f42a
Marina H <mxhuisken@gmail.com>
2022-07-31 14:46:54 -0700

    finish product

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
+      port: process.env.SQL_PORT || 8889,
       dialectOptions: {
         decimalNumbers: true,
       },
